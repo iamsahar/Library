f430f333a326215459672a9c97c6be8076a3c8d1
Sahar Farahzad <shr.farahzad@gmail.com>
2020-11-19 11:04:39 +0330

    model and admin created

--- books/admin.py
+++ books/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Book
 
 # Register your models here.
+
+
+# class BookAdmin(admin.ModelAdmin):
+#     fields = [
+#         'title',
+#         'author',
+#         'publication_date',
+#         'description',
+#     ]
+#
+#     class Meta:
+#         model = Book
+
+admin.site.register(Book) #, BookAdmin
--- books/models.py
+++ books/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.utils import timezone
 
 # Create your models here.
+User = get_user_model()
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, limit_choices_to={'username': True}, )
+    publication_date = models.DateField(auto_now=False, auto_now_add=False, default=timezone.now)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
