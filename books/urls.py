f1f5d8d16ca7ed4f946763b44743e58219116d64
Sahar Farahzad <shr.farahzad@gmail.com>
2020-11-19 15:03:56 +0330

    books_views changed to methods from functions

--- books/urls.py
+++ books/urls.py
@@ -1,11 +1,16 @@
 from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
 urlpatterns = [
-    path('', views.books_overview, name="books_overview"),
-    path('book_list/', views.book_list, name="book_list"),
-    path('book_detail/<str:pk>/', views.book_detail, name="book_detail"),
-    path('book_create/', views.book_create, name="book_create"),
-    path('book_update/<str:pk>/', views.book_update, name="book_update"),
-    path('book_delete/<str:pk>/', views.book_delete, name="book_delete"),
+    path('books/', views.BookList.as_view()),
+    path('books/<int:pk>/', views.BookDetail.as_view()),
+    # path('', views.books_overview, name="books_overview"),
+    # path('book_list/', views.book_list, name="book_list"),
+    # path('book_detail/<str:pk>/', views.book_detail, name="book_detail"),
+    # path('book_create/', views.book_create, name="book_create"),
+    # path('book_update/<str:pk>/', views.book_update, name="book_update"),
+    # path('book_delete/<str:pk>/', views.book_delete, name="book_delete"),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- books/views.py
+++ books/views.py
@@ -8,69 +8,120 @@ from .models import Book
 
 # Create your views here.
 
-@api_view(['GET'])
-def books_overview(request):
-    """
-    A view that returns the URL pattern that we want to setup.
-    """
-    api_urls = {
-        'List': '/book-list/',
-        'Detail View': '/book-detail/<str:pk>/',
-        'Create': '/book-create/',
-        'Update': '/book-update/<str:pk>/',
-        'Delete': '/book-delete/<str:pk>/',
-    }
-    return Response(api_urls)
+from django.http import Http404
+from rest_framework.views import APIView
+from rest_framework import status
 
 
-@api_view(['GET'])
-def book_list(request):
+class BookList(APIView):
     """
-    A view that returns the list of books.
+    List all books, or create a new book.
     """
-    books = Book.objects.all()
-    serializer = BookSerializer(books, many=True)
-    return Response(serializer.data)
+    def get(self, request, format=None):
+        books = Book.objects.all()
+        serializer = BookSerializer(books, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = BookSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-@api_view(['GET'])
-def book_detail(request, pk):
+class BookDetail(APIView):
     """
-    A view that returns the detail of the created book.
+    Retrieve, update or delete a book instance.
     """
-    tasks = Book.objects.get(id=pk)
-    serializer = BookSerializer(tasks, many=False)
-    return Response(serializer.data)
+    def get_object(self, pk):
+        try:
+            return Book.objects.get(pk=pk)
+        except Book.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        book = self.get_object(pk)
+        serializer = BookSerializer(book)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        book = self.get_object(pk)
+        serializer = BookSerializer(book, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        book = self.get_object(pk)
+        book.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
-@api_view(['POST'])
-def book_create(request):
-    """
-    A view that creates a book.
-    """
-    serializer = BookSerializer(data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-    return Response(serializer.data)
-
-
-@api_view(['POST'])
-def book_update(request, pk):
-    """
-    A view that edits a book's detail.
-    """
-    task = Book.objects.get(id=pk)
-    serializer = BookSerializer(instance=task, data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-    return Response(serializer.data)
-
-
-@api_view(['DELETE'])
-def book_delete(request, pk):
-    """
-    A view that deletes a book.
-    """
-    task = Book.objects.get(id=pk)
-    task.delete()
-    return Response('Item successfully deleted!')
+# @api_view(['GET'])
+# def books_overview(request):
+#     """
+#     A view that returns the URL pattern that we want to setup.
+#     """
+#     api_urls = {
+#         'List': '/book-list/',
+#         'Detail View': '/book-detail/<str:pk>/',
+#         'Create': '/book-create/',
+#         'Update': '/book-update/<str:pk>/',
+#         'Delete': '/book-delete/<str:pk>/',
+#     }
+#     return Response(api_urls)
+#
+#
+# @api_view(['GET'])
+# def book_list(request):
+#     """
+#     A view that returns the list of books.
+#     """
+#     books = Book.objects.all()
+#     serializer = BookSerializer(books, many=True)
+#     return Response(serializer.data)
+#
+#
+# @api_view(['GET'])
+# def book_detail(request, pk):
+#     """
+#     A view that returns the detail of the created book.
+#     """
+#     tasks = Book.objects.get(id=pk)
+#     serializer = BookSerializer(tasks, many=False)
+#     return Response(serializer.data)
+#
+#
+# @api_view(['POST'])
+# def book_create(request):
+#     """
+#     A view that creates a book.
+#     """
+#     serializer = BookSerializer(data=request.data)
+#     if serializer.is_valid():
+#         serializer.save()
+#     return Response(serializer.data)
+#
+#
+# @api_view(['POST'])
+# def book_update(request, pk):
+#     """
+#     A view that edits a book's detail.
+#     """
+#     task = Book.objects.get(id=pk)
+#     serializer = BookSerializer(instance=task, data=request.data)
+#     if serializer.is_valid():
+#         serializer.save()
+#     return Response(serializer.data)
+#
+#
+# @api_view(['DELETE'])
+# def book_delete(request, pk):
+#     """
+#     A view that deletes a book.
+#     """
+#     task = Book.objects.get(id=pk)
+#     task.delete()
+#     return Response('Item successfully deleted!')
