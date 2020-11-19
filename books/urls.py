56e5b748cb7f9c73f9d71df4dbc748f71a3e9b6b
Sahar Farahzad <shr.farahzad@gmail.com>
2020-11-19 12:49:42 +0330

    books list created

--- books/urls.py
+++ books/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.books_overview, name="books_overview"),
+    path('book_list/', views.book_list, name="book_list"),
 ]
--- books/views.py
+++ books/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .serializers import BookSerializer
+from .models import Book
 
 
 # Create your views here.
@@ -15,5 +17,11 @@ def books_overview(request):
         'Update': '/book-update/<str:pk>/',
         'Delete': '/book-delete/<str:pk>/',
     }
-
     return Response(api_urls)
+
+
+@api_view(['GET'])
+def book_list(request):
+    books = Book.objects.all()
+    serializer = BookSerializer(books, many=True)
+    return Response(serializer.data)
